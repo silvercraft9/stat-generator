9d8502ea589767fa2f3a83a1dbb61a87464766e2
k2r <roland.kottokombi@gmail.com>
2020-03-24 19:38:48 +0100

    Initialization of ExcelFileparser. Renaming of IFileReader interface.

---
+++ src/main/java/parser/ExcelFileParser.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package parser;
+
+import java.util.ArrayList;
+
+import core.IRecord;
+
+/**
+ * @author ro6k4
+ *
+ */
+public class ExcelFileParser implements IFileParser {
+
+	@Override
+	public String getFilePath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<IRecord> getRecords() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/main/java/parser/IFileParser.java
+++ src/main/java/parser/IFileParser.java
@@ -11,7 +11,7 @@ import core.IRecord;
  * @author ro6k4
  *
  */
-public interface IFileReader {
+public interface IFileParser {
 	
 	/**
 	 * 
