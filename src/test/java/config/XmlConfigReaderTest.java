6c77e3b5f786bc59f863cdb373e8d1dab1029c8b
k2r <roland.kottokombi@gmail.com>
2020-03-24 19:31:58 +0100

    Complete tests for XmlConfigReader. Modification of example config file.

--- src/test/java/config/XmlConfigReaderTest.java
+++ src/test/java/config/XmlConfigReaderTest.java
@@ -11,8 +11,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import core.Category;
 import core.Domain;
+import core.Field;
+import core.ICategory;
 import core.IDomain;
+import core.IField;
+import core.ISubCategory;
+import core.SubCategory;
 
 /**
  * @author ro6k4
@@ -84,7 +90,29 @@ class XmlConfigReaderTest {
 	 */
 	@Test
 	void testGetCategories() {
-		fail("Not yet implemented");
+		ArrayList<ICategory> categories = new ArrayList<ICategory>();
+		
+		Category cat1 = Mockito.mock(Category.class);
+		Mockito.when(cat1.getName()).thenReturn("CategorieA");
+		
+		Category cat2 = Mockito.mock(Category.class);
+		Mockito.when(cat2.getName()).thenReturn("CategorieB");
+		
+		Category cat3 = Mockito.mock(Category.class);
+		Mockito.when(cat3.getName()).thenReturn("CategorieC");
+		
+		categories.add(cat1);
+		categories.add(cat2);
+		categories.add(cat3);
+
+		ArrayList<ICategory> actualCategories = confReader.getCategories();
+		int n = actualCategories.size();
+		for(int i = 0; i < n; i++) {
+			ICategory actual = actualCategories.get(i);
+			ICategory expected = categories.get(i);
+			assertEquals(expected.getName(), actual.getName());
+		}
+
 	}
 
 	/**
@@ -92,7 +120,56 @@ class XmlConfigReaderTest {
 	 */
 	@Test
 	void testGetSubCategories() {
-		fail("Not yet implemented");
+		Category cat1 = Mockito.mock(Category.class);
+		Mockito.when(cat1.getName()).thenReturn("CategorieA");
+		
+		Category cat2 = Mockito.mock(Category.class);
+		Mockito.when(cat2.getName()).thenReturn("CategorieB");
+		
+		Category cat3 = Mockito.mock(Category.class);
+		Mockito.when(cat3.getName()).thenReturn("CategorieC");
+		
+		ArrayList<ISubCategory> subcats = new ArrayList<ISubCategory>();
+		
+		SubCategory subcat1 = Mockito.mock(SubCategory.class);
+		Mockito.when(subcat1.getName()).thenReturn("Sous_Categorie1");
+		Mockito.when(subcat1.getParent()).thenReturn(cat1);
+		
+		SubCategory subcat2 = Mockito.mock(SubCategory.class);
+		Mockito.when(subcat2.getName()).thenReturn("Sous_Categorie2");
+		Mockito.when(subcat2.getParent()).thenReturn(cat1);
+		
+		SubCategory subcat3 = Mockito.mock(SubCategory.class);
+		Mockito.when(subcat3.getName()).thenReturn("Sous_Categorie3");
+		Mockito.when(subcat3.getParent()).thenReturn(cat2);
+		
+		SubCategory subcat4 = Mockito.mock(SubCategory.class);
+		Mockito.when(subcat4.getName()).thenReturn("Sous_Categorie4");
+		Mockito.when(subcat4.getParent()).thenReturn(cat3);
+		
+		SubCategory subcat5 = Mockito.mock(SubCategory.class);
+		Mockito.when(subcat5.getName()).thenReturn("Sous_Categorie5");
+		Mockito.when(subcat5.getParent()).thenReturn(cat3);
+		
+		SubCategory subcat6 = Mockito.mock(SubCategory.class);
+		Mockito.when(subcat6.getName()).thenReturn("Sous_Categorie6");
+		Mockito.when(subcat6.getParent()).thenReturn(cat3);
+		
+		subcats.add(subcat1);
+		subcats.add(subcat2);
+		subcats.add(subcat3);
+		subcats.add(subcat4);
+		subcats.add(subcat5);
+		subcats.add(subcat6);
+		
+		ArrayList<ISubCategory> actualSubCats = confReader.getSubCategories();
+		int n = actualSubCats.size();
+		for(int i = 0; i < n; i++) {
+			ISubCategory actual = actualSubCats.get(i);
+			ISubCategory expected = subcats.get(i);
+			assertEquals(expected.getName(), actual.getName());
+			assertEquals(expected.getParent().getName(), actual.getParent().getName());
+		}
 	}
 
 	/**
@@ -100,7 +177,48 @@ class XmlConfigReaderTest {
 	 */
 	@Test
 	void testGetFields() {
-		fail("Not yet implemented");
+		ArrayList<IField> fields = new ArrayList<IField>();
+		
+		IField field1 = Mockito.mock(Field.class);
+		Mockito.when(field1.getName()).thenReturn("ChampX");
+		Mockito.when(field1.getType()).thenReturn("brut");
+		Mockito.when(field1.getRank()).thenReturn(1);
+		
+		IField field2 = Mockito.mock(Field.class);
+		Mockito.when(field2.getName()).thenReturn("ChampY");
+		Mockito.when(field2.getType()).thenReturn("brut");
+		Mockito.when(field2.getRank()).thenReturn(2);
+		
+		IField field3 = Mockito.mock(Field.class);
+		Mockito.when(field3.getName()).thenReturn("ChampZ");
+		Mockito.when(field3.getType()).thenReturn("brut");
+		Mockito.when(field3.getRank()).thenReturn(3);
+		
+		IField field4 = Mockito.mock(Field.class);
+		Mockito.when(field4.getName()).thenReturn("ChampN");
+		Mockito.when(field4.getType()).thenReturn("precalcule");
+		Mockito.when(field4.getRank()).thenReturn(1);
+		
+		IField field5 = Mockito.mock(Field.class);
+		Mockito.when(field5.getName()).thenReturn("ChampO");
+		Mockito.when(field5.getType()).thenReturn("precalcule");
+		Mockito.when(field5.getRank()).thenReturn(2);
+		
+		fields.add(field1);
+		fields.add(field2);
+		fields.add(field3);
+		fields.add(field4);
+		fields.add(field5);
+		
+		ArrayList<IField> actualFields = confReader.getFields();
+		int n = actualFields.size();
+		for(int i = 0; i < n; i++) {
+			IField actual = actualFields.get(i);
+			IField expected = fields.get(i);
+			assertEquals(expected.getName(), actual.getName());
+			assertEquals(expected.getType(), actual.getType());
+			assertEquals(expected.getRank(), actual.getRank());
+		}
 	}
 
 }
